1e270028a2e51f48b8f8a79fc344e729cf5d3eb5
Gustavo Flores <gustavobtflores@gmail.com>
2025-01-22 00:57:31 -0300

    feat: ajuste para reproduzir som do fim do warmup no tempo correto

--- src/content-scripts/lobby/partidaInfo.js
+++ src/content-scripts/lobby/partidaInfo.js
@@ -38,7 +38,7 @@ export const buildTimer = ( warmupFinished, targetContainer, timeleft, maxtime =
       $( '#warmup_timer' ).html( warmupFinished );
     }
 
-    if ( timeleft <= 60 ) {
+    if ( distance <= 60 && timeleft > 0 ) {
       tocarSomFimWarmup();
     }
   }, 1000 );
